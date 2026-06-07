c70dbf4e6c64bba90242c404224e7065e75a35f3
DzikriyanBaikHati <mdzikriyansyah@gmail.com>
2026-06-07 23:43:20 +0700

    Update app. py Kelompok NGULANG.py

--- app. py Kelompok NGULANG.py
+++ app. py Kelompok NGULANG.py
@@ -1,5 +1,58 @@
 import streamlit as st
 
+import streamlit as st
+
+st.set_page_config(
+    page_title="ChemLearN Hub",
+    page_icon="🧪",
+    layout="wide"
+)
+
+st.markdown("""
+<style>
+
+.stApp{
+    background: linear-gradient(
+    135deg,
+    #0f172a,
+    #1e293b,
+    #111827);
+    color:white;
+}
+
+.main-title{
+    text-align:center;
+    font-size:55px;
+    font-weight:bold;
+    background: linear-gradient(90deg,#38bdf8,#22c55e,#f97316);
+    -webkit-background-clip:text;
+    -webkit-text-fill-color:transparent;
+}
+
+.card{
+    background: rgba(255,255,255,0.08);
+    backdrop-filter: blur(15px);
+    padding:25px;
+    border-radius:20px;
+    border:1px solid rgba(255,255,255,0.2);
+    margin-bottom:20px;
+}
+
+.card:hover{
+    transform:scale(1.02);
+    transition:0.3s;
+}
+
+.badge{
+    background:#22c55e;
+    padding:5px 12px;
+    border-radius:20px;
+    color:white;
+    font-weight:bold;
+}
+
+</style>
+""", unsafe_allow_html=True)
 # Sidebar Menu
 menu = st.sidebar.selectbox(
     "Pilih menu",
