968ff9f736a34801cd8cc7fa760ed35f51edfff2
DzikriyanBaikHati <mdzikriyansyah@gmail.com>
2026-06-07 23:44:28 +0700

    Update app. py Kelompok NGULANG.py

--- app. py Kelompok NGULANG.py
+++ app. py Kelompok NGULANG.py
@@ -63,7 +63,7 @@ menu = st.sidebar.selectbox(
 if menu == "Beranda":
 
     st.markdown(
-        '<p class="main-title">🧪 CHEMLAB INTERACTIVE</p>',
+        '<p class="main-title">🧪 ChemLearN HUB</p>',
         unsafe_allow_html=True
     )
 
